7ef9d27827a3ffd73d2ff164010e3e7cdccce58b
Tom Kidd <tomkidd@gmail.com>
2020-01-13 07:21:56 -0600

    Reorganized, added basic tvOS target

---
+++ VVVVVV-tvOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VVVVVV-tvOS
+//
+//  Created by Tom Kidd on 1/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
